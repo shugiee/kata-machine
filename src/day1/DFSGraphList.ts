ea151679f50c6c09769dcd83ca34f84b86c58a84
Jay <jonathanholson@gmail.com>
2024-10-03 17:15:38 -0400

    Completed DFSGraphList

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,40 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(
+    graph: WeightedAdjacencyList, 
+    curr: number, 
+    needle: number, 
+    seen: boolean[], 
+    path: number[]): boolean {
 
+    if (seen[curr]) {
+        return false;
+    }
+
+    seen[curr] = true;
+    path.push(curr);
+
+    if (curr === needle) {
+        return true;
+    }
+
+    for (const adj of graph[curr]) {
+        const hasFoundNeedle = walk(graph, adj.to, needle, seen, path);
+        if (hasFoundNeedle) {
+            return true;
+        }
+    }
+
+    path.pop();
+    return false;
+}
+export default function dfs(
+    graph: WeightedAdjacencyList, 
+    source: number, 
+    needle: number): number[] | null {
+    const seen: boolean[] = [];
+    const path: number[] = [];
+    walk(graph, source, needle, seen, path);
+    if (path.length === 0) {
+        return null;
+    }
+    return path;
 }
