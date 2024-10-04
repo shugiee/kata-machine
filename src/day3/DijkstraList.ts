4dc03f8b17c6345c19cafcb91f9d8334a7d9d5bd
Jay <jonathanholson@gmail.com>
2024-10-03 21:37:32 -0400

    Completed Dijkstras, naive approach

---
+++ src/day3/DijkstraList.ts
@@ -0,0 +1,62 @@
+function hasUnvisitedNodes(visitedNodes: boolean[]) {
+    return visitedNodes.includes(false);
+}
+
+function getLowestUnvisitedNode(visitedNodes: boolean[], dists: number[]) {
+    let lowestIndex = -1;
+    let lowestDist = Infinity;
+
+    for (let i = 0; i < visitedNodes.length; i++) {
+        if (visitedNodes[i] === false && dists[i] < lowestDist) {
+            lowestDist = dists[i];
+            lowestIndex = i;
+        }
+    }
+
+    return lowestIndex;
+}
+
+export default function dijkstra_list(
+    source: number, 
+    sink: number, 
+    arr: WeightedAdjacencyList): number[] {
+        const visitedNodes = new Array(arr.length).fill(false);
+        const dists = new Array(arr.length).fill(Infinity);
+        const prev = new Array(arr.length).fill(-1);
+        dists[source] = 0;
+
+        while (hasUnvisitedNodes(visitedNodes)) {
+            const currNode = getLowestUnvisitedNode(visitedNodes, dists);
+            if (currNode === -1) {
+                throw new Error("Couldn't find a lowest unvisited node!");
+            }
+            visitedNodes[currNode] = true;
+            if (currNode === sink) {
+                break;
+            }
+
+            for (const edge of arr[currNode]) {
+                const newDist = dists[currNode] + edge.weight;
+                if (newDist < dists[edge.to]) {
+                    dists[edge.to] = newDist;
+                    prev[edge.to] = currNode;
+                }
+
+            }
+        }
+
+        if (dists[sink] === -1) {
+            return [];
+        }
+
+        const path: number[] = [];
+        let curr = sink;
+
+        while (curr !== source) {
+            path.push(curr);
+            curr = prev[curr];
+        }
+        path.push(source);
+
+        return path.reverse();
+    }
